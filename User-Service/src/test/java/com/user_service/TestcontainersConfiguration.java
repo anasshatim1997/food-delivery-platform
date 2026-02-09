9ee03bb392b701be1f749b50f38b894216223f52
anasshatim1997 <anass1997hatim@gmail.com>
2026-02-09 22:30:34 +0100

    Add eureka server , and service discovery

--- User-Service/src/test/java/com/user_service/TestcontainersConfiguration.java
+++ User-Service/src/test/java/com/user_service/TestcontainersConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -12,7 +12,7 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
+    PostgreSQLContainer postgresContainer() {
 		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
 	}
 
