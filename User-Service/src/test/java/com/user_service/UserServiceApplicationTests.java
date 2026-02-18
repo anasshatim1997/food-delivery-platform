afe89bfafba174a913c3cc5ce7bd532e670aa098
anasshatim1997 <anass1997hatim@gmail.com>
2026-02-18 01:10:09 +0000

    Add eureka server , and service discovery

--- User-Service/src/test/java/com/user_service/UserServiceApplicationTests.java
+++ User-Service/src/test/java/com/user_service/UserServiceApplicationTests.java
@@ -2,14 +2,14 @@ package com.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
