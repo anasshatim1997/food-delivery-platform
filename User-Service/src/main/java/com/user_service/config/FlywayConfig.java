4395db84089aa3f1b73cb1988bacfa321cc5d6f5
anasshatim1997 <anass1997hatim@gmail.com>
2026-02-22 13:42:30 +0000

    Develop the oauth Service , Storage service

--- User-Service/src/main/java/com/user_service/config/FlywayConfig.java
+++ User-Service/src/main/java/com/user_service/config/FlywayConfig.java
@@ -1,6 +1,7 @@
 package com.user_service.config;
 
 import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import javax.sql.DataSource;
 public class FlywayConfig {
 
     @Bean(initMethod = "migrate")
+    @ConditionalOnProperty(name = "spring.flyway.enabled", havingValue = "true", matchIfMissing = true)
     public Flyway flyway(DataSource dataSource) {
         return Flyway.configure()
                 .dataSource(dataSource)
